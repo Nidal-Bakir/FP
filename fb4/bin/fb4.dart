073c9c91908eb434e60d422fe6ce3b1354df6c5a
Nidal Bakir <nidal.bakir.edu@gmail.com>
2023-09-24 23:14:08 +0300

    init

---
+++ fb4/bin/fb4.dart
@@ -0,0 +1,5 @@
+import 'package:fb4/fb4.dart' as fb4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fb4.calculate()}!');
+}
---
+++ fb4/test/fb4_test.dart
@@ -0,0 +1,8 @@
+import 'package:fb4/fb4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
