4eb2c525c485ef9a3a56806cf931855b49b30991
Nidal Bakir <nidal.bakir.edu@gmail.com>
2023-09-27 15:29:59 +0300

    fb5 compose functions

---
+++ fp5/fp5.dart
@@ -0,0 +1,26 @@
+void main(List<String> arguments) {
+  const list = [1, 2, 3, 4, 5];
+
+  var res = list.map(addOne).map(square).map(subtract10).join(' , ');
+  print(res);
+  print('======================');
+
+  res = list.map((x) => subtract10(square(addOne(x)))).join(' , ');
+  print(res);
+  print('======================');
+
+  res = list.map(addOneSquareSubtract10()).join(' , ');
+  print(res);
+  print('======================');
+}
+
+int Function(int x) addOneSquareSubtract10() =>
+    addOne.compose(square).compose(subtract10);
+
+int addOne(int x) => x + 1;
+int square(int x) => x * x;
+int subtract10(int x) => x - 10;
+
+extension FunctionCompose<I, M> on M Function(I) {
+  O Function(I) compose<O>(O Function(M) g) => (I x) => g(this(x));
+}
